cdd70911a49f345542c940673938fb03cb9ba494
paul <paul>
2023-01-31 20:01:11 -0500

    connection script

--- routes/sql.js
+++ routes/sql.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const createError = require('http-errors');
-// const sql = require('mssql');
-const sql = require('mssql/msnodesqlv8');
+const sql = require('mssql');
+// const sql = require('mssql/msnodesqlv8');
+
+const Connection = require('tedious').Connection;
+const  Request = require('tedious').Request;
+
 const router = express.Router();
 
 let config;
@@ -9,40 +13,34 @@ let config;
 config = {
     user: '',
     password: '' ,
-    server: '(localdb)\MSSQLLocalDB',
-    database: 'olmsted_SAHD_MDB'
+    host: 'localhost',
+    r: '(localdb)\MSSQLLocalDB',
+    server: 'localhost',
+    options: {
+        database: 'olmsted_SAHD_MDB'
+    }
 };
 
 // config = 'data source=ORION-II\LOCALDB#F8A9A27C;initial catalog=olmsted_SAHD_MDB;trusted_connection=true';
 // config = 'Server=(localdb)\MSSQLLocalDB;Integrated Security=true';
+// config = 'Server=localhost\MSSQLLocalDB;Database=olmsted_SAHD_MDB;trusted_connection=true';
 
 router.all('/', function(req, res, next) {
     res.send('MS SQL, baby!');
 });
 
 router.get('/connect', (req, res, next) => {
-    sql.connect(config, function (err) {
-        if (err) {
+    const connection = new Connection(config);
+    // console.log('connection:', connection);
+
+    connection.on('connect', (err) => {
+        if(err) {
             console.log(err);
-            return next( createError(500, 'Problem!!') );
+        } else {
+            console.log('Connected!');
         }
-
-        // create Request object
-        // var request = new sql.Request();
-           
-        // query to the database and get the records
-        /*
-        request.query('select * from Student', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-        */
-
-        res.send('OK!');
     });
+    
+    res.send('OK!');
 });
 module.exports = router;
---
+++ testing/connectionsql.js
@@ -0,0 +1,52 @@
+//We require mssql package for this sample
+var sqlcon = require('mssql');
+
+// console.log({sqlcon});
+
+function GetSQLData(queryCallback){        //A callback function is taken as an argument. Once the operation is completed we will be calling this
+   
+    //SQL Configuration
+    var config = {
+        user:''            //SQL User Id. Please provide a valid user
+        ,password:''    //SQL Password. Please provide a valid password
+        ,r:'localhost\\SQL2K14'
+        ,server: '(localdb)\MSSQLLocalDB'
+        /*
+            Since my SQL is an instance I am using 'localhost\\Instance'.
+            If you have SQL installed on the default instance, it should be server:'localhost'
+        */
+        ,database: 'olmsted_SAHD_MDB'        //You can use any database here
+    };
+
+    var connection = new sqlcon.ConnectionPool(config, function(err) {
+        //In case of an error print the error to the console. You can have your customer error handling
+        if (err) console.log(err);
+       
+        //Query Database
+        var dbQuery = new sqlcon.Request(connection);
+        //Purposely we are delaying the results
+        dbQuery.query("WAITFOR DELAY '00:00:05';SELECT * FROM INFORMATION_SCHEMA.TABLES",function(err,resultset){
+            //In case of an error print the error to the console. You can have your customer error handling
+            if (err) console.log(err);
+           
+            //Passing the resultset to the callback function
+            queryCallback(resultset);
+        })
+    });
+}
+function callback (resultset){
+    console.dir('Results returned and printed from the call back function');
+    console.dir(resultset);
+   
+    //Exit the application
+    console.dir('Exiting the Application');
+    process.exit(0);
+}
+//Calling the function
+console.dir('Calling GetSQLData');
+GetSQLData(callback);
+/*
+    Once we call this function even there's a delay to return the results
+    you will see the next line printing 'Waiting for callback function to get invoked...'
+*/
+console.dir('Waiting for callback function to get invoked...');
